9acfb749d8aba4928579b26c818c54dcc08170fa
jeg <steholl@posteo.org>
2023-09-20 00:50:54 +0200

    junit test HomeController

--- src/test/java/com/stevedutch/intellectron/web/HomeControllerTest.java
+++ src/test/java/com/stevedutch/intellectron/web/HomeControllerTest.java
@@ -2,41 +2,35 @@ package com.stevedutch.intellectron.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 class HomeControllerTest {
 
-	@InjectMocks
-	HomeController underTest;
-
+	//according to DanVega, just junit
 	@Test
-	void testShowHomePage() {
-		fail("Not yet implemented");
+	void testShowHomePageJustCode() {
+		HomeController sut = new HomeController();
+		String result = sut.showHomePage();
+		
+		assertThat(result).isEqualTo("/welcome");
 	}
-
+	
 	@Test
 	void testShowAlsoHomePage() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	 void testShowHomePage_1() throws Exception {
-	// given
-	// when
-	String actual=underTest.showHomePage();
-	// then
-	assertThat(actual).isEqualTo("TestExpected");
-	}
-
-	@Test
-	 void testShowAlsoHomePage_1() throws Exception {
-	// given
-	// when
-	String actual=underTest.showAlsoHomePage();
-	// then
-	assertThat(actual).isEqualTo("TestExpected");
+		HomeController sut = new HomeController();
+		String result = sut.showAlsoHomePage();
+		
+		assertThat(result).isEqualTo("/welcome");
 	}
 
 }
