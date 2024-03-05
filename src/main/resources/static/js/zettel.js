806006811a21d52e5e9653a0abc899f7d13ea176
jeg <steholl@posteo.org>
2024-03-05 01:15:26 +0100

    add modal for delete Zettel

--- src/main/resources/static/js/zettel.js
+++ src/main/resources/static/js/zettel.js
@@ -4,3 +4,23 @@ document.getElementById('change').addEventListener('click', function () {
     const zettelData = zettel.innerHTML;
     zettel.innerHTML = '<form action="/input" method="post" th:object="${ZettelDtoRecord}">' + zettelData + '</form>';
    });
+
+   //get the modal
+const modal = document.getElementById("deleteModal");
+//open modal
+document.getElementById('openDeleteModal').addEventListener('click', function () {
+    modal.style.display = "block";
+});
+
+let closeModal = document.getElementById("cancel");
+// When the user clicks on cancel, close the modal
+closeModal.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
