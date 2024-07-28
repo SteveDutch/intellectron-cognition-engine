e56b9d888538765a9f4c5e1d1ec79b8cbaa67960
jeg <steholl@posteo.org>
2024-07-28 21:39:45 +0200

    refactor SearchServiceTest

---
+++ src/test/java/com/stevedutch/intellectron/service/ZettelServiceTest.java
@@ -0,0 +1,14 @@
+package com.stevedutch.intellectron.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ZettelServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
