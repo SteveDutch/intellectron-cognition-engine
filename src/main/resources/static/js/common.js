30c75781f10147ff9673900329d4ff8d8de861cb
jeg <steholl@posteo.org>
2024-10-06 11:31:45 +0200

    show searchTerm function in common.js

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,11 +1,20 @@
 // Aktuelle URL abrufen
 const url = window.location.href;
-
-// URL in Teile zerlegen
-const urlParts = new URL(url);
-
-// Hostnamen extrahieren
-const searchTerm = urlParts.search;
+//
+//// URL in Teile zerlegen
+//const urlParts = new URL(url);
+//
+//// Hostnamen extrahieren
+//const searchTerm = urlParts.search;
 
 // Hostnamen in das HTML-Element einfügen
-document.getElementById('searchTerm').textContent = searchTerm;
+document.getElementById('searchTerm').textContent = getValueAfterEquals(url);
+
+
+function getValueAfterEquals(url) {
+    const equalsIndex = url.lastIndexOf('=');
+    if (equalsIndex !== -1 && equalsIndex < url.length - 1) {
+        return url.substring(equalsIndex + 1);
+    }
+}
+
