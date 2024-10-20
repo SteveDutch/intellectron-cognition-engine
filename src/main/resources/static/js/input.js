a43be7820562ee4eb6400f0bb458e75f29edf5de
jeg <steholl@posteo.org>
2024-10-20 13:01:17 +0200

    refactor zettel.js & input.js, last round

--- src/main/resources/static/js/input.js
+++ src/main/resources/static/js/input.js
@@ -5,47 +5,6 @@ let zettel = {};
 let note = {};
 let author = {};
 
-/* document.getElementById('addTagButton').addEventListener('click', function () {
-    var tagsContainer = document.getElementById('tagsContainer');
-    var newDiv = document.createElement('div');
-    var newInput = document.createElement('input');
-    newInput.type = 'text';
-    newInput.name = 'tags'; // Name attribute to bind the input to an ArrayList
-    var newRemoveButton = document.createElement('button');
-	newRemoveButton.classList.add('button', 'is-small', 'is-warning');
-    newRemoveButton.type = 'button';
-    newRemoveButton.textContent = 'rydde ut Tag';
-    newRemoveButton.onclick = function () {
-        this.parentNode.remove();
-    }
-    tagsContainer.appendChild(newDiv);
-    newDiv.appendChild(newInput);
-    newDiv.appendChild(newRemoveButton);
-}); */
-
-
-/* document.getElementById('addReferenceButton').addEventListener('click', function () { 
-    var signatureContainer = document.getElementById('signatureContainer');
-    var newDiv = document.createElement('div');
-    var newReferenceInput = document.createElement('input');
-    newReferenceInput.type = 'number';
-    newReferenceInput.min = '100000000000';
-    newReferenceInput.max = '999999999999';
-    newReferenceInput.pattern = '[0-9]{12}';
-    newReferenceInput.setAttribute('required', 'required');
-    newReferenceInput.name = 'references'; // Name attribute to bind the input to an ArrayList
-    var newRemoveRefButton = document.createElement('button');
-    newRemoveRefButton.type = 'button';
-	newRemoveRefButton.classList.add('button', 'is-small', 'is-warning');
-    newRemoveRefButton.textContent = 'rydde ut referanse';
-    newRemoveRefButton.onclick = function () {
-        this.parentNode.remove();
-    }
-    signatureContainer.appendChild(newDiv);
-    newDiv.appendChild(newReferenceInput);
-    newDiv.appendChild(newRemoveRefButton);
-}); */
-
 submitBtn.addEventListener("click", function (event) {
 	// suppress HTML sending form
 	event.preventDefault();
